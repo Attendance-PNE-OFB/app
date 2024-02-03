bd8c0423faa881915a67d659a9b70dd9c145fee7
FlorianMachenaud <145437462+FlorianMachenaud@users.noreply.github.com>
2024-02-03 14:29:20 +0100

    Update, sous forme de fonction c'est plus simple

--- extractMetadata.py
+++ extractMetadata.py
@@ -2,65 +2,66 @@ import exiftool
 import re
 
 
-human_pattern = r'^humain.*'
-man_pattern = r'^homme.*'
-woman_pattern = r'^femme.*'
-age1_pattern = r'^0-15.*'
-age2_pattern = r'^15-35.*'
-age3_pattern = r'^35-60.*'
-age4_pattern = r'^>60.*'
-direction_pattern = r'^droite|gauche'
+def extractMetadata(file_path):
+    human_pattern = r'^humain.*'
+    man_pattern = r'^homme.*'
+    woman_pattern = r'^femme.*'
+    age1_pattern = r'^0-15.*'
+    age2_pattern = r'^15-35.*'
+    age3_pattern = r'^35-60.*'
+    age4_pattern = r'^>60.*'
+    direction_pattern = r'^droite|gauche'
 
-result = {} # dictionnaire regroupant les noms des images, avec pour chacun leurs informations sous forme de dictionnaire (data)
-data = {} # disctionnaire regroupant les informations de chaque image
-genre = [] # liste de genre [nb_humains, nb_hommes, nb_femmes]
-cat_age = [] # liste de catégorie d'âge [0-15, 15-35, 35-60, >60]
+    result = {} # dictionnaire regroupant les noms des images, avec pour chacun leurs informations sous forme de dictionnaire (data)
+    data = {} # disctionnaire regroupant les informations de chaque image
+    genre = [] # liste de genre [nb_humains, nb_hommes, nb_femmes]
+    cat_age = [] # liste de catégorie d'âge [0-15, 15-35, 35-60, >60]
 
-with exiftool.ExifTool() as et:
+    with exiftool.ExifTool() as et:
+        # Obtention des métadonnées de toutes les images du dossier séléctionné
+        metadata = et.execute_json("-json", "-r", "-ext", "jpg", file_path)
 
-    # Obtention des métadonnées de toutes les images du dossier séléctionné
+    for i in range(len(metadata)):
+        data = {}
+        genre = []
+        cat_age = []
 
-    # Indiquer ici le dossier vers les images
-    metadata = et.execute_json("-json", "-r", "-ext", "jpg", "dossier/vers/images")
+        if 'XMP:Subject' in metadata[i]:
+            for j in metadata[i]['XMP:Subject']:
 
-for i in range(len(metadata)):
-    data = {}
-    genre = []
-    cat_age = []
+                if metadata[i]['XMP:Subject'].index(j) == 0:
+                    data['type'] = j # ajout du type d'activité
 
-    if 'XMP:Subject' in metadata[i]:
-        for j in metadata[i]['XMP:Subject']:
+                #-------------------Partie sexe-------------------
+                if re.match(human_pattern, j):
+                    genre.append(j[-2:])
+                elif re.match(man_pattern, j):
+                    genre.append(j[-2:])
+                elif re.match(woman_pattern, j):
+                    genre.append(j[-2:])
+                elif len(genre) < 3:
+                    genre.append('0')
 
-            if metadata[i]['XMP:Subject'].index(j) == 0:
-                data['type'] = j # ajout du type d'activité
+                #-------------Partie Catégorie d'âge-------------
+                if re.match(age1_pattern, j):
+                    cat_age.append(j)
+                elif re.match(age2_pattern, j):
+                    cat_age.append(j)
+                elif re.match(age3_pattern, j):
+                    cat_age.append(j)
+                elif re.match(age4_pattern, j):
+                    cat_age.append(j)
 
-            #-------------------Partie sexe-------------------
-            if re.match(human_pattern, j):
-                genre.append(j[-2:])
-            elif re.match(man_pattern, j):
-                genre.append(j[-2:])
-            elif re.match(woman_pattern, j):
-                genre.append(j[-2:])
-            elif len(genre) < 3:
-                genre.append('0')
+                #----------------Partie Direction----------------
+                if re.match(direction_pattern, j):
+                    #print("direction:", j)
+                    direction = j
+                    data['direction'] = j
 
-            #-------------Partie Catégorie d'âge-------------
-            if re.match(age1_pattern, j):
-                cat_age.append(j)
-            elif re.match(age2_pattern, j):
-                cat_age.append(j)
-            elif re.match(age3_pattern, j):
-                cat_age.append(j)
-            elif re.match(age4_pattern, j):
-                cat_age.append(j)
+            data['genre'] = genre
+            data['age'] = cat_age
+        result[metadata[i]['File:FileName']] = data
+    return result
 
-            #----------------Partie Direction----------------
-            if re.match(direction_pattern, j):
-                #print("direction:", j)
-                direction = j
-                data['direction'] = j
+print(extractMetadata('../photos/sur'))
 
-        data['genre'] = genre
-        data['age'] = cat_age
-    result[metadata[i]['File:FileName']] = data
-print(result)
