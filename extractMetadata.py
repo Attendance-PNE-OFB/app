9ff3fa2f49289a2cff9ff7d1087aaba6f757c706
FlorianMachenaud <145437462+FlorianMachenaud@users.noreply.github.com>
2024-02-05 11:12:51 +0100

    modifs simples

--- extractMetadata.py
+++ extractMetadata.py
@@ -3,31 +3,32 @@ import exiftool
 import re
 import json
 
-def extractMetadata(file_path):
+
+def extract_metadata(file_path):
     human_pattern = r'^humain.*'
     man_pattern = r'^homme.*'
     woman_pattern = r'^femme.*'
     age_pattern = r'^(((<|>)\d{1,2})|(\d{1,2}-\d{1,2}))ans$'
     direction_pattern = r'^droite|gauche'
 
-    result = {} # dictionnaire regroupant les noms des images, avec pour chacun leurs informations sous forme de dictionnaire (data)
+    result = {}  # dictionnaire regroupant les noms des images, avec leurs informations sous forme de dictionnaire
 
     with exiftool.ExifTool() as et:
         # Obtention des métadonnées de toutes les images du dossier séléctionné
         metadata = et.execute_json("-json", "-r", "-ext", "jpg", file_path)
 
     for i in range(len(metadata)):
-        data = {} # disctionnaire regroupant les informations de chaque image
-        genre = [] # liste de genre [nb_humains, nb_hommes, nb_femmes]
-        cat_age = [] # liste de catégorie d'âge [0-15, 15-35, 35-60, >60]
+        data = {}  # disctionnaire regroupant les informations de chaque image
+        genre = []  # liste de genre [nb_humains, nb_hommes, nb_femmes]
+        cat_age = []  # liste de catégorie d'âge [0-15, 15-35, 35-60, >60]
 
         if 'XMP:Subject' in metadata[i]:
             for j in metadata[i]['XMP:Subject']:
 
                 if metadata[i]['XMP:Subject'].index(j) == 0:
-                    data['type'] = j # ajout du type d'activité
+                    data['type'] = j  # ajout du type d'activité
 
-                #-------------------Partie sexe-------------------
+                # -------------------Partie sexe-------------------
                 if re.match(human_pattern, j):
                     genre.append(j[-2:])
                 elif re.match(man_pattern, j):
@@ -37,11 +38,11 @@ def extractMetadata(file_path):
                 elif len(genre) < 3:
                     genre.append('0')
 
-                #-------------Partie Catégorie d'âge-------------
+                # -------------Partie Catégorie d'âge-------------
                 if re.match(age_pattern, j):
                     cat_age.append(j)
 
-                #----------------Partie Direction----------------
+                # ----------------Partie Direction----------------
                 if re.match(direction_pattern, j):
                     data['direction'] = j
 
@@ -49,8 +50,10 @@ def extractMetadata(file_path):
             data['age'] = cat_age
         result[metadata[i]['File:FileName']] = data
     return result
-def dictionnary_to_json(dict):
-    f = CreateUnicCsv('output/metadata.json')
+
+
+def dictionary_to_json(dict):
+    f = create_unic_file('output/metadata.json')
     f.write("{\n")
     for key in dict:
         json_obj = json.dumps(key, indent=0)
@@ -70,11 +73,12 @@ def dictionnary_to_json(dict):
     f.write("}")
     f.close()
 
-def CreateUnicCsv(filename):
+
+def create_unic_file(filename):
     base_name, extension = os.path.splitext(filename)
     counter = 0
-    if not os.path.exists('output/'):
-        os.makedirs('output/')
+    if not os.path.exists('output_json/'):
+        os.makedirs('output_json/')
     while os.path.exists(filename):
         counter += 1
         filename = f"{base_name}_{counter}{extension}"
@@ -82,6 +86,8 @@ def CreateUnicCsv(filename):
     f = open(filename, 'w')
     return f
 
-dict = (extractMetadata('lien/vers/dossier/photos'))
-dictionnary_to_json(dict)
 
+# Extraction des métadonnées des images du dossier 'sur'
+res = (extract_metadata('../photos/sur'))
+# Conversion du dictionnaire en fichier json
+dictionary_to_json(res)
